3932c79094ed0a96d37b50a7f218c77d7e087d1f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-20 19:02:53 -0300

    Saved roxanne/main.py at Timestamp: 2018-07-20 22:02:53

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,8 +1,15 @@
 # hedy.roxanne.main.py
-from browser import document, alert, html
-from _spy.vitollino.main import Cena, STYLE
+from _spy.vitollino.main import Cena, STYLE, Texto, Elemento
 STYLE['width'] = 740
-noite = "https://imgur.com/Cfqvyhg"
+STYLE['height'] = 740
+
+
+NOITE = "https://i.imgur.com/Cfqvyhg.png"
+
+#def _main():
+ #   document['pydiv'].html = ""
+  #  a_noite = Cena(img=NOITE)
+   # a_noi.vai()
 
 
 #if __name__ == "__main__":
