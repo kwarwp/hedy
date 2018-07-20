d1b0d8ca2c5b8d10e1cc91201390697704755734
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-20 18:28:00 -0300

    Created roxanne/main.py at Timestamp: 2018-07-20 21:28:00

---
+++ roxanne/main.py
@@ -0,0 +1,10 @@
+# hedy.roxanne.main.py
+from browser import document, alert, html
+from _spy.vitollino.main import Cena, STYLE
+STYLE['width'] = 740
+noite = "https://imgur.com/Cfqvyhg"
+
+
+#if __name__ == "__main__":
+    #main()
+    #Bloco(oce, 3, 7)
