a98d3bf2608e13a0cc253165e2e9a87fca9625cc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 11:41:12 -0300

    Saved kellee/__log__.py at Timestamp: 2018-08-29 14:41:12

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -73,3 +73,21 @@ Exception: <SyntaxError: Expected ';'>
     criarcenas()  
 NameError: name 'criarcenas' is not defined
 '''},
+{'date': 'Wed Aug 29 2018 11:41:12.705 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 56
+    criarcena()  
+  module <module> line 19
+    mesadecha2 = Cena(img=MESADECHA2)
+NameError: name 'MESADECHA2' is not defined
+'''},
