eb278f78f6d1f925cba52e8e1719969586d87466
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 11:24:10 -0300

    Created kellee/__log__.py at Timestamp: 2018-08-29 14:24:10

---
+++ kellee/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 29 2018 11:24:09.71 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  HOMEMARANHA "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjSmreJp5LdAhXDHZAKHY29A4cQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F574771971170218088%2F&psig=AOvVaw0zzyBdsssUOQiqUfvLLmSY&ust=1535633484620048"
+               ^
+SyntaxError: invalid syntax
+'''},
