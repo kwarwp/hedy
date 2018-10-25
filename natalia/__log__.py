fe02b48f6a8f51eaf395a4919882c45fe33f48b1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 11:53:18 -0300

    Created natalia/__log__.py at Timestamp: 2018-10-25 14:53:18

---
+++ natalia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Oct 25 2018 11:53:30.621 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from lisa.main import Rodada()
+                                ^
+SyntaxError: invalid syntax
+'''},
