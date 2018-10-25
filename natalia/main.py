9f2b8ed986734f4791ea5f91df47602a950c0d79
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 11:53:02 -0300

    Created natalia/main.py at Timestamp: 2018-10-25 14:53:02

---
+++ natalia/main.py
@@ -0,0 +1,43 @@
+# hedy.natalia.main.zpy
+from lisa.main import Rodada()
+from lisa.main import Partida()
+from ruzwana.main import Ficar()
+from ruzwana.main import Turno()
+from kathryn.main import Joias()
+from lisa.main import Morrer()
+
+"""
+kathryn - Baralho - Joias - Rodada
+callie - Perigo - Perder - Tesouro
+ruzwana - Ficar - Turno - Sair 
+natalia - Mesa - Iniciar - Jogador 
+lisa - Decisao - Morrer - Partida
+meredith - Universo 
+"""
+
+
+class Iniciar: 
+    def __init__(self):
+        print("Iniciar __init__")
+        self.rodada = Rodada()
+    def comeca(self)
+        self.rodada.inicia()
+        
+class Jogador:
+    def __init__(self):
+        self.ficar = Ficar()
+        self.sair = Sair()
+        
+class Mesa: 
+    def __init__(self): 
+        self.partida = Partida()
+        self.jogador = Jogador()
+        self.turno = Turno()
+        self.joias = Joias()
+        self.morrer = Morrer()
+        
+if __natalia__ == "__main__": 
+    iniciar = Iniciar()
+    iniciar.comeca()
+
+
