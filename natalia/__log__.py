ac548dc25053562052bb1462fe12c7196e358e72
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 11:58:12 -0300

    Saved natalia/__log__.py at Timestamp: 2018-10-25 14:58:11

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -66,3 +66,19 @@ NameError: name '__natalia__' is not defined
     if __natalia__ == "__main__": 
 NameError: name '__natalia__' is not defined
 '''},
+{'date': 'Thu Oct 25 2018 11:58:25.53 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 40
+    if __natalia__ == "__main__": 
+NameError: name '__natalia__' is not defined
+'''},
