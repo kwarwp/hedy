02507b1d45fcb70cf883e26412e32ddea1cfad58
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 12:05:48 -0300

    Saved natalia/__log__.py at Timestamp: 2018-10-25 15:05:48

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -105,3 +105,10 @@ NameError: name '__natalia__' is not defined
                    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Oct 25 2018 12:06:02.955 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 39
+  def receba(self)
+                   ^
+SyntaxError: invalid syntax
+'''},
