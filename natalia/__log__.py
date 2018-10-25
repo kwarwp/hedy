7fc23026a6c99c0d947510b8f03307bd04745eed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 11:53:42 -0300

    Saved natalia/__log__.py at Timestamp: 2018-10-25 14:53:42

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -6,3 +6,10 @@
                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Oct 25 2018 11:53:56.568 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from kathryn.main import Rodada()
+                                   ^
+SyntaxError: invalid syntax
+'''},
