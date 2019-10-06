d678465b2a143c10f01a05cf08283477a40720ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-06 09:47:55 -0300

    Created roxanne/__log__.py @2019-10-06 12:47

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sun Oct 06 2019 09:46:35.105 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  self.cena_n  = Cena(img = noite) 
+  ^
+IndentationError: expected an indented block
+'''},
