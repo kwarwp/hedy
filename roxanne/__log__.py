18ff0af09699851233ebf301eca9dd1345ecb56c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-07 14:45:47 -0300

    Saved roxanne/__log__.py @2019-11-07 17:45

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -6,3 +6,21 @@
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Thu Nov 07 2019 15:46:35.161 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 7
+    class praiouuu():
+  module <module> line 12
+    banhista.entra.sul(praia)
+AttributeError: 'method' object has no attribute 'sul'
+'''},
