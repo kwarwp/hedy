c1795ad65b7b27bb8c4ba90b076de47e26bb9fef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-07 14:35:31 -0300

    Saved roxanne/main.py @2019-11-07 17:35

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,1 +1,6 @@
 # hedy.roxanne.main.py
+from _spy.vitollino.main import Cena,Elemento
+from _spy.vitollino.main import INVENTARIO as inv
+PRAIA="https://img.freepik.com/fotos-gratis/praia-e-no-ceu-uma-bela-ilha-tropical_36076-482.jpg?size=626&ext=jpg"
+SOL="
+BANHISTA="
